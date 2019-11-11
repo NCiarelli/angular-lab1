b8ed93693b6dad39d828855797e9a3cb0ce5a481
Ncia89 <nicholas.ciarelli@gmail.com>
2019-11-11 16:28:26 -0500

    All basic requirements of part 2 completed. Todo list is now interactive. Items can be added, filtered, removed, and marked as completed. Working on making them editable.

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -21,4 +21,40 @@ export class TodoComponent implements OnInit {
     { task: "be awesome", completed: false }
   ];
 
+  taskFilter: string = "";
+  todoToAdd: Todo = {
+    task: "",
+    completed: false
+  };
+
+  // function to add tasks to the to-do list from the to-do-list form
+  addTask(): void {
+    this.displayedToDos.push(this.todoToAdd);
+    this.todoToAdd = { task: "", completed: false };
+  }
+
+  // Function to mark a task as completed and remove the complete button (done through a CSS class linked to the completed flag)
+  completeTask(taskCompleted: Todo): void {
+    taskCompleted.completed = true;
+  }
+
+  // Function to remove a task from the page entirely (used on clicking the X button of a todo item)
+  removeTask(taskToRemove: Todo): void {
+    const indexOfTodo: number = this.displayedToDos.indexOf(taskToRemove);
+    this.displayedToDos.splice(indexOfTodo, 1);
+  }
+
+  // Function to filter the displayed todo items, used by the filter input text field
+  // Included toLowerCase calls to make the filtering case insensitive
+  filterTodoItems(): Todo[] {
+    const filterText: string = this.taskFilter;
+    const filteredList = this.displayedToDos.filter(function (currentItem) {
+      return currentItem.task.toLowerCase().includes(filterText.toLowerCase());
+    });
+    return filteredList;
+  }
+
+  clearFilter(): void {
+    this.taskFilter = "";
+  }
 }
